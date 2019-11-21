5cc99db1ba9f742613d901130f8c3882147ca88d
tony <tony.spicer.covin@gmail.com>
2019-11-20 18:25:01 -0600

    remove key

--- app/src/main/java/com/tony/weatherapp/MainActivity.java
+++ app/src/main/java/com/tony/weatherapp/MainActivity.java
@@ -13,16 +13,9 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener, ForecastFragment.OnFragmentInteractionListener {
 
-    private String WEATHER_KEY = "9db10307657b0ff8224b0da642ac57f7";
+
     private String TAG = "MAIN_ACTIVITY";
 
-    private Button mShowWeather;
-    private EditText mGetCity;
-
-
-
-
-    MainFragment mainFragment;
 
     public MainActivity() {
         //empty constructer
